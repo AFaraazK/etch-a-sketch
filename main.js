dde730bde7a6c1c159d75d2ddf83009debc481f7
Az <kfaraazali@gmail.com>
2022-07-29 05:18:57 -0400

    Add button functionality and make grid size variable

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 const grid = document.querySelector(".grid");
+let cursorColor = "black";
 
+function createGrid(size){    
+    grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size},1fr)`;
 
-for(let i=0;i<256;i++){
-    let square = document.createElement("div");
-    square.style.backgroundColor = "white";
+    for(let i=0;i<size**2;i++){
+        let square = document.createElement("div");
+        square.classList.add("gridSquare");
+        square.style.backgroundColor = "white";
 
-    // eventListener for mouse hover
-    // > change color of div to black
-    square.addEventListener("mouseover", e => {
-        square.style.backgroundColor = "black";
-    });
+        // eventListener for mouse hover
+        // > change color of div to black
+        square.addEventListener("mouseover", e => {
+            square.style.backgroundColor = `${cursorColor}`;
+        });
 
-    grid.insertAdjacentElement('beforeend',square);
+        grid.insertAdjacentElement('beforeend',square);
+    }
 }
+
+function changeColor(newColor){
+    cursorColor = newColor;
+}
+
+function reset(){
+    const board = document.querySelector(".grid");
+    let squares = board.querySelectorAll(".gridSquare");
+    squares.forEach((div) =>(div.style.backgroundColor = "white"));
+}
+
+function changeGridSize(){
+    let newSize = prompt("New Size: ");
+    reset();
+    createGrid(newSize);
+}
+
+createGrid(16);
