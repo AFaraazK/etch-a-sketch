f63ab2962b6ef954fa021a45b112933286beed70
Az <kfaraazali@gmail.com>
2022-07-28 20:02:05 -0400

    Add mouseOver functionality

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ const grid = document.querySelector(".grid");
 for(let i=0;i<256;i++){
     let square = document.createElement("div");
     square.style.backgroundColor = "white";
+
+    // eventListener for mouse hover
+    // > change color of div to black
+    square.addEventListener("mouseover", e => {
+        square.style.backgroundColor = "black";
+    });
+
     grid.insertAdjacentElement('beforeend',square);
 }
